43ec3a65c1bcbe1b9fcf82d51af7c82b5f43849b
fffrrrf <joeybuckleton@gmail.com>
2026-06-28 22:55:35 +0100

    version alpha_0.11
    •added user (non saving)
    •fixed liveram temporarily
    •added new command: whoami

--- terminal_alpha.py
+++ terminal_alpha.py
@@ -1,19 +1,26 @@
-#import
+#import the stuff
 
 import psutil
 import sys
 
 print("boot")
-
+usrtrue = 0
+while not usrtrue:
+    usr = input("Enter your username")
+    if len(usr) <= 10:
+        usrtrue = 1
+    else: print("Username must be 10 or less characters.")
+#loop for it
 running = 1
 while running:
    print('')
-   cmd =  input()
+   cmd =  input(f"{usr}@pyggy:~$ ")
    if cmd == "ver":
-       print("alpha0.1")
+       print("alpha_0.11")
    elif cmd == "raminf":
       mem = psutil.virtual_memory()
-      print(mem.percent, "in use")
+      print(mem.percent, "in use of", mem.total)
+      
    elif cmd == "esc":
        sys.exit() 
    elif cmd == "cpuinf":
@@ -24,8 +31,18 @@ while running:
        print("esc")
        print("cpuinf")
        print("help")
+   elif cmd == "whoami":
+       print(usr)
    elif cmd == "liveram":
        ramactike = 1
+       bean = 1
        while ramactike:
+           #this crap really annoyed me
            mem = psutil.virtual_memory()
-           print(f"\rram: {mem.percent}%", end="")
+           print(f"\rram: {mem.percent}%",
+            end="")
+           bean -= 0.00002
+           if bean <= 0:
+               ramactike = 0
+   else:
+        print(f"Unkown command: {cmd}")
